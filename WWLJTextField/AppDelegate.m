e654da5b5639c81700d0d9daae03dd89aabfd3ab
WuWenJie <826505807@qq.com>
2015-07-14 20:28:07 +0800

    Update AppDelegate.m

--- WWLJTextField/AppDelegate.m
+++ WWLJTextField/AppDelegate.m
@@ -2,8 +2,8 @@
 //  AppDelegate.m
 //  WWLJTextField
 //
-//  Created by iShareme on 15/7/1.
-//  Copyright (c) 2015年 iShareme. All rights reserved.
+//  Created by 武文杰 on 15/7/1.
+//  Copyright (c) 2015年 武文杰. All rights reserved.
 //
 
 #import "AppDelegate.h"
